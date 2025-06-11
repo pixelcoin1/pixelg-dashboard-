7cf6ba26dd80e07db3a0e9f96f06e67f72ce8519
pixelcoin1 <emrekudu57@gmail.com>
2025-06-11 23:14:28 +0200

    Add files via upload

---
+++ pixelg-dashboard-fixed122/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="PixelG Logo" />
+        <h1>Willkommen beim PixelG Coin Dashboard</h1>
+        <p>Dein Wallet-Interface für PXG Token</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
