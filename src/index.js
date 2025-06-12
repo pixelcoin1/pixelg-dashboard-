9b1f7faaa267d8329e678c04f3c10482f75818c8
pixelcoin1 <emrekudu57@gmail.com>
2025-06-12 16:34:47 +0200

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import PixelGDashboard from "./PixelGDashboard";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<PixelGDashboard />);
