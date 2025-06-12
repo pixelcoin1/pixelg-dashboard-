d264f78cc4c6f2f02609a596205e7b45f3032dc0
pixelcoin1 <emrekudu57@gmail.com>
2025-06-12 16:56:27 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import PixelGDashboard from "./App";
+import PixelGDashboard from "./PixelGDashboard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<PixelGDashboard />);
